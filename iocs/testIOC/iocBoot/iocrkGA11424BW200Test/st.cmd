c807051d783a7b23685ebde4073f6097016eaffe
Jeremy Lorelli <lorelli@slac.stanford.edu>
2023-11-01 16:05:30 -0700

    restructure iocs/ subdirectory

--- iocs/testIOC/iocBoot/iocrkGA11424BW200Test/st.cmd
+++ iocs/testIOC/iocBoot/iocrkGA11424BW200Test/st.cmd
@@ -31,7 +31,7 @@ drvModbusAsynConfigure("${AMP_PORT}_InRegs", "${AMP_PORT}", 0, 3, 1, 11, UINT16,
 drvModbusAsynConfigure("${AMP_PORT}_OutRegs", "${AMP_PORT}", 0, 16, 1, 4, UINT16, 500, "")
 
 # Finally, load the records. P is record prefix, PORT is the asyn port
-dbLoadRecords("${rkGA11424BW200}/db/GA11424BW200.db", "P=amp1:, INPORT=${AMP_PORT}_InRegs, OUTPORT=${AMP_PORT}_OutRegs, SCAN='.1 second'")
+dbLoadRecords("${RKGA11424BW200}/db/GA11424BW200.db", "P=amp1:, INPORT=${AMP_PORT}_InRegs, OUTPORT=${AMP_PORT}_OutRegs, SCAN='.1 second'")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
